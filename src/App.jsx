2b724063a35cf6257941fc63251ab639ab23c51f
josellecallora08 <josellecallora08@gmail.com>
2024-09-12 20:58:00 +0800

    did the handson

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import InstantConsultation from "./InstantConsultationBooking/InstantConsultation";
+import BookingConsultation from "./components/BookingConsultation";
 function App() {
   return (
     <>
       <Router>
         <Routes>
-          <Route path="/" element={<InstantConsultation />} />
+          <Route path="/instant-consultation" element={<BookingConsultation />} />
         </Routes>
       </Router>
     </>
